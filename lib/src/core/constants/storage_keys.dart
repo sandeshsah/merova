6b6687693d22bf847b5127e3110aff6c54afde17
Sandesh Sah <sahsandesh2056@gmail.com>
2026-02-11 10:31:59 +0545

    feat: improve the ui of auth folder login, forgot, otp, reset.

---
+++ lib/src/core/constants/storage_keys.dart
@@ -0,0 +1,12 @@
+class StorageKeys {
+  static const String fullName = 'fullName';
+  static const String userEmail = 'userEmail';
+  static const String userId = 'userId';
+  static const String phoneNumber = 'phoneNumber';
+  static const String userPassword = 'userPassword';
+  static const String accessToken = 'access_token';
+  static const String remember = 'remember';
+  static const String isEmail = 'isEmail';
+  static const String countryCode = 'countryCode';
+  static const String countryFlag = 'countryFlag';
+}
---
+++ lib/src/core/routes/app_router.gr.dart
@@ -0,0 +1,334 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+part of 'app_router.dart';
+
+abstract class _$AppRouter extends RootStackRouter {
+  // ignore: unused_element
+  _$AppRouter({super.navigatorKey});
+
+  @override
+  final Map<String, PageFactory> pagesMap = {
+    ForgotPasswordRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const ForgotPasswordPage(),
+      );
+    },
+    FundTransferRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const FundTransferPage(),
+      );
+    },
+    HomeRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const HomePage(),
+      );
+    },
+    LoginRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const LoginPage(),
+      );
+    },
+    OnboardingRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const OnboardingPage(),
+      );
+    },
+    OtpRoute.name: (routeData) {
+      final args = routeData.argsAs<OtpRouteArgs>();
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: OtpPage(
+          flow: args.flow,
+          emailOrPhone: args.emailOrPhone,
+        ),
+      );
+    },
+    PasswordResetSuccessRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const PasswordResetSuccessPage(),
+      );
+    },
+    PaymentRoutes.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const PaymentPages(),
+      );
+    },
+    ProfileRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const ProfilePage(),
+      );
+    },
+    RegisterRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const RegisterPage(),
+      );
+    },
+    ResetPasswordRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const ResetPasswordPage(),
+      );
+    },
+    SplashRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const SplashPage(),
+      );
+    },
+    WelcomeRoute.name: (routeData) {
+      final args = routeData.argsAs<WelcomeRouteArgs>();
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: WelcomePage(
+          key: args.key,
+          uid: args.uid,
+        ),
+      );
+    },
+  };
+}
+
+/// generated route for
+/// [ForgotPasswordPage]
+class ForgotPasswordRoute extends PageRouteInfo<void> {
+  const ForgotPasswordRoute({List<PageRouteInfo>? children})
+      : super(
+          ForgotPasswordRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'ForgotPasswordRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [FundTransferPage]
+class FundTransferRoute extends PageRouteInfo<void> {
+  const FundTransferRoute({List<PageRouteInfo>? children})
+      : super(
+          FundTransferRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'FundTransferRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [HomePage]
+class HomeRoute extends PageRouteInfo<void> {
+  const HomeRoute({List<PageRouteInfo>? children})
+      : super(
+          HomeRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'HomeRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [LoginPage]
+class LoginRoute extends PageRouteInfo<void> {
+  const LoginRoute({List<PageRouteInfo>? children})
+      : super(
+          LoginRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'LoginRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [OnboardingPage]
+class OnboardingRoute extends PageRouteInfo<void> {
+  const OnboardingRoute({List<PageRouteInfo>? children})
+      : super(
+          OnboardingRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'OnboardingRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [OtpPage]
+class OtpRoute extends PageRouteInfo<OtpRouteArgs> {
+  OtpRoute({
+    required String flow,
+    String? emailOrPhone,
+    List<PageRouteInfo>? children,
+  }) : super(
+          OtpRoute.name,
+          args: OtpRouteArgs(
+            flow: flow,
+            emailOrPhone: emailOrPhone,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'OtpRoute';
+
+  static const PageInfo<OtpRouteArgs> page = PageInfo<OtpRouteArgs>(name);
+}
+
+class OtpRouteArgs {
+  const OtpRouteArgs({
+    required this.flow,
+    this.emailOrPhone,
+  });
+
+  final String flow;
+
+  final String? emailOrPhone;
+
+  @override
+  String toString() {
+    return 'OtpRouteArgs{flow: $flow, emailOrPhone: $emailOrPhone}';
+  }
+}
+
+/// generated route for
+/// [PasswordResetSuccessPage]
+class PasswordResetSuccessRoute extends PageRouteInfo<void> {
+  const PasswordResetSuccessRoute({List<PageRouteInfo>? children})
+      : super(
+          PasswordResetSuccessRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'PasswordResetSuccessRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [PaymentPages]
+class PaymentRoutes extends PageRouteInfo<void> {
+  const PaymentRoutes({List<PageRouteInfo>? children})
+      : super(
+          PaymentRoutes.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'PaymentRoutes';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [ProfilePage]
+class ProfileRoute extends PageRouteInfo<void> {
+  const ProfileRoute({List<PageRouteInfo>? children})
+      : super(
+          ProfileRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'ProfileRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [RegisterPage]
+class RegisterRoute extends PageRouteInfo<void> {
+  const RegisterRoute({List<PageRouteInfo>? children})
+      : super(
+          RegisterRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'RegisterRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [ResetPasswordPage]
+class ResetPasswordRoute extends PageRouteInfo<void> {
+  const ResetPasswordRoute({List<PageRouteInfo>? children})
+      : super(
+          ResetPasswordRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'ResetPasswordRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [SplashPage]
+class SplashRoute extends PageRouteInfo<void> {
+  const SplashRoute({List<PageRouteInfo>? children})
+      : super(
+          SplashRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'SplashRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [WelcomePage]
+class WelcomeRoute extends PageRouteInfo<WelcomeRouteArgs> {
+  WelcomeRoute({
+    Key? key,
+    required String uid,
+    List<PageRouteInfo>? children,
+  }) : super(
+          WelcomeRoute.name,
+          args: WelcomeRouteArgs(
+            key: key,
+            uid: uid,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'WelcomeRoute';
+
+  static const PageInfo<WelcomeRouteArgs> page =
+      PageInfo<WelcomeRouteArgs>(name);
+}
+
+class WelcomeRouteArgs {
+  const WelcomeRouteArgs({
+    this.key,
+    required this.uid,
+  });
+
+  final Key? key;
+
+  final String uid;
+
+  @override
+  String toString() {
+    return 'WelcomeRouteArgs{key: $key, uid: $uid}';
+  }
+}
